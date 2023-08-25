11ec44fec608041f5986f13137af86b8bd43f200
Xeostop1 <stop1@daum.net>
2023-08-25 15:18:26 +0900

    edit:검색기능수정

--- static/js/index.js
+++ static/js/index.js
@@ -1,79 +1,102 @@
 $(document).ready(function () {
+  // 주소 데이터 저장
   let fetchedAddresses = [];
-  let timeoutId = null;
+  // 이전 사용자 입력값 저장
+  let previousInput = "";
 
-  // 화면 초기화 함수
-  const resetView = () => {
-    console.log("화면 초기화");
-    $("#suggested-addresses-list").empty();
-    $("#zipcode").val("");
-  };
+  // 타임아웃 ID 저장
+  let timeoutId = null;
 
   // 주소 데이터 요청 함수
   const getAddressData = function (input) {
-    console.log("주소 데이터 요청");
-    return new Promise((resolve, reject) => {
-      $.ajax({
-        url: "/search_address",
-        method: "GET",
-        dataType: "json",
-        data: { query: input },
-        success: resolve,
-        error: reject,
-      });
+    console.log("주소 데이터를 요청합니다: ", input);
+    return $.ajax({
+      url: "/search_address",
+      method: "GET",
+      dataType: "json",
+      data: { query: input },
+    }).done(function () {
+      console.log("주소 데이터 요청이 완료되었습니다.");
     });
   };
 
   // 화면 업데이트 함수
   const updateView = (data) => {
-    console.log("화면 업데이트");
+    console.log("화면을 업데이트합니다.");
+
+    $("#suggested-addresses-list").empty();
+
     if (data.error) {
-      console.log("검색 결과가 없습니다.");
-      $("#suggested-addresses-list").empty().append(`<li>${data.error}</li>`);
+      $("#suggested-addresses-list").append(`<li>${data.error}</li>`);
       return;
     }
 
-    //json꼭 살펴보기!
-    console.log("검색된 주소 출력");
-    data.forEach(function (address, index) {
+    data.forEach(function (address) {
       const street = address.street;
       const city = address.city;
       const state = address.state;
       const zipcode = address.zipcode;
-      const addressItem = $(
-        `<li>${street}, ${city}, ${state} (${zipcode})</li>`
-      );
 
-      console.log(`주소 항목 생성 - ${street}, ${city}, ${state} (${zipcode})`);
-      addressItem.on("click", function () {
-        console.log(`주소 선택 - ${street}, ${city}, ${state} (${zipcode})`);
-        $("#zipcode").val(zipcode);
-      });
+      let addressItem = $("<li>")
+        .text(`${street} ${city} ${state} (${zipcode})`)
+        .click(function () {
+          $("#zipcode").val(zipcode);
+          $("#street").val(`${street} ${city} ${state}`);
+          console.log(`${street} ${city} ${state} (${zipcode})`);
+        });
 
       $("#suggested-addresses-list").append(addressItem);
     });
   };
 
-  // 주소 입력 이벤트 처리
-  $("#street").on("input", async function () {
+  $("#street").on("input", function () {
     clearTimeout(timeoutId);
 
-    const inputValue = $(this).val().trim();
+    let inputValue = $(this).val().trim();
+
+    console.log(`사용자 입력 처리 - '${inputValue}'`);
+
     if (!inputValue) {
-      resetView();
+      $("#suggested-addresses-list").empty();
       return;
     }
 
-    // 주소 검색 지연 처리 1초
-    timeoutId = setTimeout(async () => {
-      try {
-        resetView();
-        const data = await getAddressData(inputValue);
+    getAddressData(inputValue)
+      .done(function (data) {
+        if (!Array.isArray(data)) {
+          if (data.error) {
+            updateView(data);
+          } else {
+            console.error("API 응답 오류: 주소 데이터 배열이 아닙니다.", data);
+          }
+
+          return;
+        }
+
         fetchedAddresses = data;
-        updateView(data);
-      } catch (error) {
-        console.log("API 호출에 실패했습니다.", error);
-      }
-    }, 1000);
+        previousInput = inputValue;
+
+        let inputWords = inputValue.toLowerCase().split(" ");
+
+        let filteredAddresses = fetchedAddresses.filter((address) => {
+          let addressString =
+            `${address.street}${address.city}${address.state}`.toLowerCase();
+
+          // 정규 표현식 생성
+          let regexPattern = new RegExp(inputWords.join(".*"), "i");
+
+          // match() 메서드를 이용해 매칭되는 주소 찾기
+          return addressString.match(regexPattern);
+        });
+
+        updateView(filteredAddresses);
+      })
+      .fail(function (jqXHR, textStatus, errorThrown) {
+        console.log("API 호출에 실패했습니다.", errorThrown);
+      });
+
+    timeoutId = setTimeout(() => {
+      console.log("입력 대기중...");
+    }, 100);
   });
 });
