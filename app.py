e27fe9dc558686616fd8122d1062f962f47dc390
Xeostop1 <stop1@daum.net>
2023-08-29 11:05:04 +0900

    edit: api 체크

--- app.py
+++ app.py
@@ -1,54 +1,11 @@
-from flask import Flask, request, jsonify
-import requests
-import logging
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-# 로그 설정
-logging.basicConfig(level=logging.INFO)
 
-
-def build_address(suggestion):
-    white_space = ""
-    if suggestion.get("secondary"):
-        if suggestion.get("entries", 0) > 1:
-            suggestion["secondary"] += f" ({suggestion['entries']} entries)"
-        white_space = " "
-    return f"{suggestion['street_line']}{white_space}{suggestion['secondary']} {suggestion['city']}, {suggestion['state']} {suggestion['zipcode']}"
-
-
-@app.route("/autocomplete", methods=["GET"])
-def autocomplete():
-    try:
-        # SmartyStreets API 키 (환경 변수 또는 직접 문자열로 입력) dl
-        api_key = "138657343210935212"
-
-        # 클라이언트에서 받은 검색어
-        search_term = request.args.get("search", "")
-
-        # 선택적 파라미터들. 필요에 따라 추가 가능.
-        max_results = request.args.get("max_results", "10")
-
-        # 기타 필요한 매개변수들을 여기에 추가하세요.
-        api_url = f"https://us-autocomplete-pro.api.smarty.com/lookup?search={search_term}&max_results={max_results}&key={api_key}"
-
-        response = requests.get(api_url)
-
-        if response.status_code != 200:
-            logging.error("API 요청에 실패했습니다.")
-            return jsonify({"error": "API 요청에 실패했습니다."}), 500
-
-        data = response.json()
-
-        for suggestion in data["suggestions"]:
-            address = build_address(suggestion)
-            print(address)
-
-        return jsonify(data)
-
-    except Exception as e:
-        logging.error(f"An error occurred: {str(e)}")
-        return jsonify({"error": "An unexpected error occurred."}), 500
+@app.route("/get-api-key", methods=["GET"])
+def get_api_key():
+    return {"api_key": "138657343210935212"}
 
 
 if __name__ == "__main__":
--- static/js/index.js
+++ static/js/index.js
@@ -6,96 +6,101 @@ $(document).ready(function () {
   const MIN_INPUT_LENGTH = 6; // 최소 입력 길이 설정
   const DELAY_TIME_MS = 5000; // 딜레이 시간 설정 (1초)
 
-  const apiKey = "138657343210935212"; //클라이언트 쪽 key 값 세팅
+  let apiKey = null;
 
-  const getAddressData = function (input) {
-    console.log("주소 데이터를 요청합니다: ", input);
-    return $.ajax({
-      url: "/autocomplete",
-      method: "GET",
-      data: { search: input, api_key: apiKey },
-    }).done(function () {
-      console.log("주소 데이터 요청이 완료되었습니다.");
-    });
-  };
-  const updateView = (data) => {
-    $("#suggested-addresses-list").empty();
+  $.get("/get-api-key", function (data) {
+    apiKey = data.api_key;
+  }).done(function () {
+    const getAddressData = function (input) {
+      console.log("주소 데이터를 요청합니다: ", input);
+      return $.ajax({
+        url: "/autocomplete",
+        method: "GET",
+        data: { search: input, api_key: apiKey },
+      }).done(function () {
+        console.log("주소 데이터 요청이 완료되었습니다.");
+      });
+    };
 
-    if (data.error) {
-      $("#suggested-addresses-list").append(`<li>${data.error}</li>`);
-
-      return;
-    }
-
-    data.forEach(function (address) {
-      const delivery_line_1 = address.delivery_line_1;
-      const last_line = address.last_line;
-      const zipcode = address.components.zipcode;
-
-      let addressItem = $("<li>")
-        .text(`${delivery_line_1}, ${last_line}`)
-        .click(function () {
-          $("#zipcode").val(zipcode);
-          $("#street").val(delivery_line_1);
-          console.log(`${delivery_line_1}, ${last_line}`);
-        });
-
-      $("#suggested-addresses-list").append(addressItem);
-    });
-  };
-
-  $("#street").on("input", function () {
-    clearTimeout(timeoutId);
-
-    let inputValue = $(this).val().trim();
-
-    console.log(`사용자 입력 처리 - '${inputValue}'`);
-
-    if (!inputValue || inputValue.length < MIN_INPUT_LENGTH) {
+    const updateView = (data) => {
       $("#suggested-addresses-list").empty();
-      return;
-    }
 
-    timeoutId = setTimeout(() => {
-      getAddressData(inputValue)
-        .done(function (data) {
-          if (!Array.isArray(data)) {
-            if (data.error) {
-              updateView(data);
-            } else {
-              console.error(
-                "API 응답 오류 : 주소 데이터 배열이 아닙니다.",
-                data
-              );
+      if (data.error) {
+        $("#suggested-addresses-list").append(`<li>${data.error}</li>`);
+
+        return;
+      }
+
+      data.forEach(function (address) {
+        const delivery_line_1 = address.delivery_line_1;
+        const last_line = address.last_line;
+        const zipcode = address.components.zipcode;
+
+        let addressItem = $("<li>")
+          .text(`${delivery_line_1}, ${last_line}`)
+          .click(function () {
+            $("#zipcode").val(zipcode);
+            $("#street").val(delivery_line_1);
+            console.log(`${delivery_line_1}, ${last_line}`);
+          });
+
+        $("#suggested-addresses-list").append(addressItem);
+      });
+    };
+
+    $("#street").on("input", function () {
+      clearTimeout(timeoutId);
+
+      let inputValue = $(this).val().trim();
+
+      console.log(`사용자 입력 처리 - '${inputValue}'`);
+
+      if (!inputValue || inputValue.length < MIN_INPUT_LENGTH) {
+        $("#suggested-addresses-list").empty();
+        return;
+      }
+
+      timeoutId = setTimeout(() => {
+        getAddressData(inputValue)
+          .done(function (data) {
+            if (!Array.isArray(data)) {
+              if (data.error) {
+                updateView(data);
+              } else {
+                console.error(
+                  "API 응답 오류 : 주소 데이터 배열이 아닙니다.",
+                  data
+                );
+              }
+
+              return;
             }
 
-            return;
-          }
-
-          fetchedAddresses = data;
-          previousInput = inputValue;
-          let inputWordsSet = new Set(inputValue.toLowerCase().split(/\s+/));
-          console.log(`입력 단어 :${[...inputWordsSet]}`);
-          let filteredAddresses = fetchedAddresses.filter((address) => {
-            let addressWordsSet = new Set(
-              `${address.street}
+            fetchedAddresses = data;
+            previousInput = inputValue;
+            let inputWordsSet = new Set(inputValue.toLowerCase().split(/\s+/));
+            console.log(`입력 단어 :${[...inputWordsSet]}`);
+            let filteredAddresses = fetchedAddresses.filter((address) => {
+              let addressWordsSet = new Set(
+                `${address.street}
                      ${address.city}
                      ${address.state}`
-                .toLowerCase()
-                .split(/\s+/)
-            );
+                  .toLowerCase()
+                  .split(/\s+/)
+              );
 
-            // Check that every word in the input is present in the
-            // set of words from the current address.
-            return [...inputWordsSet].every((word) =>
-              addressWordsSet.has(word)
-            );
+              // Check that every word in the input is present in the
+              // set of words from the current address.
+              return [...inputWordsSet].every((word) =>
+                addressWordsSet.has(word)
+              );
+            });
+            updateView(filteredAddresses);
+          })
+          .fail(function (jqXHR, textStatus, errorThrown) {
+            console.error("API 호출에 실패했습니다.", errorThrown);
           });
-          updateView(filteredAddresses);
-        })
-        .fail(function (jqXHR, textStatus, errorThrown) {
-          console.error("API 호출에 실패했습니다.", errorThrown);
-        });
-    }, DELAY_TIME_MS);
+      }, DELAY_TIME_MS);
+    });
   });
 });
